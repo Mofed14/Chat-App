30ee895be41a636a67c0e12c900cf49c3086a004
mofed <mofed.salah.hana@gmail.com>
2023-04-22 13:11:39 +0200

    send message and get notification, send location and get notification

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,13 +5,21 @@ socket.on("message", (message) => {
 });
 
 const form = document.getElementById("form");
-const input = document.querySelector("#input");
+const input = form.querySelector("#input");
+const formButton = form.querySelector("#form-button");
+let sendLocation = document.querySelector("#send-location");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+
   if (input.value) {
     // the third argument is an acknowledgements
     socket.emit("sendMessage", input.value, (error) => {
+      // To delete message from input after sending
+      input.value = "";
+      // and we'll call focus to move the cursor inside of there.
+      input.focus();
+
       if (error) {
         return console.log(error);
       }
@@ -21,14 +29,24 @@ form.addEventListener("submit", function (e) {
 });
 
 // Send Location
-document.querySelector("#send-location").addEventListener("click", () => {
+sendLocation.addEventListener("click", () => {
   if (!navigator.geolocation)
     return alert("Geolocation is not supported on your browser");
 
+  // Disable the button untill the location is sent
+  sendLocation.setAttribute("disabled", "disabled");
+
   navigator.geolocation.getCurrentPosition((position) => {
-    const { latitude, longitude } = position.coords;
+    let { latitude, longitude } = position.coords;
+
     // callback the third argument is a socket acknowledgement
     socket.emit("sendLocation", { latitude, longitude }, () => {
+      // enable after acknowledgement
+      // sendLocation.removeAttribute("disabled");
+
+      // latitude = "";
+      // longitude = "";
+
       console.log("Location shared!");
     });
   });
